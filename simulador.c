ae245b7a833f779fb368fc2e40c2e654f1b94ac5
Arthur Augusto Claro Sardella <arthursardella@tecgraf.puc-rio.br>
2023-12-07 18:49:36 -0300

    Convergido main.c e simulador.c implementado parte da getPhysAddr

--- simulador.c
+++ simulador.c
@@ -1,34 +1,142 @@
+#define SIM 1
+
 #include "simulador.h"
-
-#include <math.h>
-
-#define BITS 32
-
-struct tabela {
-    Pagina* tbl;
-};
 
 struct pagina {
     unsigned int addressPhis; // ARM00000 00000XXX XXXXXXXX XXXXXXXX
-    int time;
+    int time; // tempo de ultimo acesso
 };
 
-typedef struct pagina Pagina;
-typedef struct tabela Tabela;
+struct quadro {
+    int num; // se -1 -> livre
+};
 
-Tabela table;
+Pagina* pagTable;
+Quadro* frameTable;
 
-void createTable(int pageSize) {
-    int qtd = BITS - calculaShift(pageSize);
-    table.tbl = (Pagina*)malloc(sizeof(Pagina) * pow(2, qtd));
-    for (int i = 0; i < qtd; i++) {
-        table.tbl->addressPhis = 0;
-        table.tbl->time = 0;
+int sizeFrameTable, sizePageTable;
+int pageSize, memSize, mode;
+int pageFaults = 0, writtenPages = 0;
+
+int clock = 0;
+
+int main(int argc, char* argv[]) {
+    if check_mode(argv[1]) {
+        error("Escolha entre os modos NRU ou LRU\n")
+    }
+
+    mode = strcmp(argv[1], "NRU") == 0 ? 0 : 1;
+    pageSize = atoi(argv[3]);
+
+    if check_size(pageSize) {
+        error("As paginas tem que ter tamanho 8 Kbytes ou 16 Kbytes\n")
+    }
+
+    memSize = atoi(argv[4]);
+    if check_mem(memSize) {
+        error("A memoria fisica tem de ser entre 1Mbytes e 4Mbytes\n")
+    }
+
+    FILE* arq = fopen(argv[2], "r");
+    if (arq == NULL) {
+        error("Arquivo nao encontrado\n")
+    }
+
+    printf("Executando o simulador...\n");
+
+    unsigned int address, page, offset = 0, physAddr;
+    char accessType;
+    int shift = calculaShift();
+    createTables();
+
+    while (!feof(arq)) {
+        fscanf(arq, "%x %c ", &address, &accessType);
+
+        page = address >> shift;
+        for (int i = 0; i < shift; i++)
+            offset += address & (0x1 << i);
+
+        physAddr = getPhysAddr(page, offset);
+
+        clock++;
+    }
+
+    fclose(arq);
+
+    printf("Arquivo de entrada: %s\n", argv[2]);
+    printf("Tamanho da memoria fisica: %d MB\n", memSize);
+    printf("Tamanho das paginas: %d KB\n", pageSize);
+    printf("Algoritmo de substituicao: %s\n", argv[1]);
+    printf("Numero de Faltas de Paginas: %d\n", pageFaults);
+    printf("Numero de Paginas Escritas: %d\n", writtenPages);
+
+    return 0;
+}
+
+void createTables() {
+    int exp = calculaShift(pageSize);
+
+    int qtd = TOTAL_BITS_SIZE - exp;
+    sizePageTable = pow(2, qtd);
+    pagTable = (Pagina*)malloc(sizeof(Pagina) * sizePageTable);
+    
+    if (pagTable == NULL) {
+        error("Espaco de memoria insuficiente\n")
+    }
+    
+    for (int i = 0; i < sizePageTable; i++) {
+        pagTable[i].addressPhis = 0;
+        pagTable[i].time = 0;
+    }
+
+    qtd = ONE_MB_EXP + memSize - exp;
+    sizeFrameTable = pow(2, qtd);
+    frameTable = (Quadro*)malloc(sizeof(Quadro) * sizeFrameTable);
+
+    if (frameTable == NULL) {
+        error("Espaco de memoria insuficiente\n")
+    }
+
+    for (int i = 0; i < sizeFrameTable; i++) {
+        frameTable[i].num = -1;
     }
 }
 
-int calculaShift(int pageSize) {
-    int base_shift = 10; //1kb
+unsigned int getPhysAddr(unsigned int index, unsigned int offset) {
+    Pagina page = pagTable[index];
+    unsigned int addr;
+    int freeFrame = -1;
+    
+    // checa se presente
+    if (!(page.addressPhis & PRESENT_BIT)) {
+        // se nao, conta mais 1 page fault
+        pageFaults++;
+
+        // acha um frame livre
+        for (int i = 0; i < sizeFrameTable; i++) {
+            if (frameTable[i].num == -1) {
+                freeFrame = i;
+                frameTable[i].num = i;
+            }
+        }
+
+        // se nao tiver frame livre -> swap
+        if (freeFrame == -1) {
+            // libera 1 e devolve numero do frame || swap();
+        }
+
+        // guarda frame na pagina
+        page.addressPhis = PRESENT_BIT + frameTable[freeFrame].num;
+    }
+
+    page.time = clock;
+    addr = page.addressPhis << calculaShift() + offset;
+
+    return addr;
+}
+
+int calculaShift() {
+    int base_shift = ONE_KB_EXP;
     double plus_shift = log2(pageSize);
     return base_shift + (int) plus_shift;
 }
--- simulador.h
+++ simulador.h
@@ -1,10 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 
 #define check_size(x) (x != 8 && x != 16)
 #define check_mem(x) (x < 1 || x > 4)
 #define check_mode(x) (strcmp(x, "LRU") && strcmp(x, "NRU"))
 #define error(msg) printf(msg); exit(1);
 
-int calculaShift(int pageSize);
+#define PRESENT_BIT 0x80000000
+
+// tamanho do endereco
+#define TOTAL_BITS_SIZE 32
+// expoente de 1 MB (2^20)
+#define ONE_MB_EXP 20
+// expoente de 1 KB (2^10)
+#define ONE_KB_EXP 10
+
+typedef struct pagina Pagina;
+typedef struct quadro Quadro;
+
+/*
+ Cria as tabelas de paginas e de quadros
+*/
+void createTables();
+/*
+ Calcula o deslocamento com base no tamanho das paginas
+*/
+int calculaShift();
+/*
+ Devolve o endereço fisico apos o mapeamento pelo simulador
+*/
+unsigned int getPhysAddr(unsigned int index, unsigned int offset);
